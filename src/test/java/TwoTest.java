8eafbc4d75c703a3dc1d2b8aae98fc7fb82334ff
marlon-nieves <marlon.3801711612@ucaldas.edu.co>
2022-11-14 16:00:56 -0500

    Add work sesion 7, reto 1 and 2

--- src/test/java/TwoTest.java
+++ src/test/java/TwoTest.java
@@ -1,36 +1,34 @@
-import Dummy.employee.request.Builder.Build;
-import Dummy.employee.request.Builder.BuildNewEmployee;
-import Dummy.employee.request.Builder.BuildNewEmployeeFake;
-import Dummy.employee.request.Builder.EmployeeRequestBuilder;
+import Dummy.employee.request.Builder.*;
 import Dummy.employee.request.EmployeeRequest;
 import Dummy.employee.response.EmployeeResponse;
-import data.factory.CreatePostDataFactory;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import model.Post.Response.PostResponse;
-import Dummy.Post.request.PostRequest;
-import Dummy.Post.response.Data;
-import Dummy.employee.request.*;
-import Dummy.Post.request.PostRequest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static data.reader.managerData.getData;
 
 public class TwoTest{
     String bearerToken="e99413b1c415ad6d592d5f2bef758493fc68c59188d9274a38219b8fc9cb5c55";
     ObjectMapper objectMapper = new ObjectMapper();
     Build buildEmployeeRequest = new Build();
     EmployeeRequestBuilder employeenew= new BuildNewEmployee();
+    EmployeeRequestBuilder employeemale = new BuildNewEmployeeMale();
     EmployeeRequestBuilder employeeFake= new BuildNewEmployeeFake();
 
     EmployeeResponse responseBodyEmployee;
@@ -40,14 +38,23 @@ public class TwoTest{
 
     @BeforeEach
     public void setup(){
+        /**Variables de ambiente para probar en ambinetes de produccion*/
+        //RestAssured.baseURI=getData("base.url");
+       // RestAssured.basePath=getData("base.path");
 
-        RestAssured.baseURI="https://dummy.restapiexample.com";
-        RestAssured.basePath="/api/v1/";
+        /**Variables de ambiente para probar en su local*/
+        RestAssured.baseURI=getData("base.url.dummy");
+        RestAssured.basePath=getData("base.path.dummy");
+
+        /**serializacion y refactorizacion de JSONCONTENT*/
         RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter());
         RestAssured.requestSpecification = new RequestSpecBuilder().setContentType(ContentType.JSON).build();
 
     }
     @Test
+    @Description("New Employee")
+    @Epic("Create EMployee")
+    @Feature("Employee")
     public void createEmployeeNew() throws JsonProcessingException {
         buildEmployeeRequest.setEmployeeRequestBuilder(employeenew);
         buildEmployeeRequest.buildEmployeeRequest();
@@ -60,7 +67,7 @@ public class TwoTest{
                         .when()
                         .post("create")
                         .then()
-                        .statusCode(200)
+                        .statusCode(201)
                         .extract().asString();
 
         responseBodyEmployee = objectMapper.readValue(response, EmployeeResponse.class);
@@ -68,6 +75,9 @@ public class TwoTest{
     }
 
     @Test
+    @Description("Employee Fake")
+    @Epic("Create EMployee")
+    @Feature("User fake")
     public void createEmployeeFake() throws JsonProcessingException {
         buildEmployeeRequest.setEmployeeRequestBuilder(employeeFake);
         buildEmployeeRequest.buildEmployeeRequest();
@@ -87,5 +97,26 @@ public class TwoTest{
         assertThat(responseBodyEmployee.getMessage(),equalTo(responseBodyEmployee.getMessage()));
 
     }
+    @Test
+    @Description("Masculino")
+    @Epic("Create user")
+    @Feature("User male")
+    public void createEmployeemale() throws JsonProcessingException {
+        buildEmployeeRequest.setEmployeeRequestBuilder(employeemale);
+        buildEmployeeRequest.buildEmployeeRequest();
+        EmployeeRequest employeeRequest = buildEmployeeRequest.getEmployeeRequest();
+        String response =
+                given()
+                        .body(employeeRequest)
+                        .header("Authorization",
+                                "Bearer " + bearerToken)
+                        .when()
+                        .post("create")
+                        .then()
+                        .statusCode(201)
+                        .extract().asString();
 
+        responseBodyEmployee = objectMapper.readValue(response, EmployeeResponse.class);
+        assertThat(responseBodyEmployee.getData().getAge(), equalTo(responseBodyEmployee.getData().getAge()));
+    }
 }
